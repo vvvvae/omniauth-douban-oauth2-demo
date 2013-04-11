05f1aaa69221120f77ea2108e440e47033ca7e5e
liluo <i@liluo.org>
2013-04-11 15:23:20 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OmniauthDoubanOauth2Demo::Application.config.secret_token = 'c09b93622c18fd613907152d9ea41cd3e9ecb0fade2b0703a54f259d2d4c6b46a3165f0a7474c8799454090e01e7556a2f88276746d3e641c43a662360a6ef64'
